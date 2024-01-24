81b594c58015bcd6672cd9e28fed3d37e337eeac
saraswat171 <saraswatchetan2020@gmail.com>
2024-01-24 14:48:41 +0530

    loginsignup

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -5,6 +5,8 @@ import TeaCollections from '../Pages/TeaCollections'
 import Accessories from '../Pages/Accessories';
 import Blog from '../Pages/Blog';
 import Contact from '../Pages/Contact';
+import Register from './register/Register.js'
+import Login from './Login/Login.js';
 
 
 function Layout() {
@@ -16,6 +18,8 @@ function Layout() {
     <Route path='/Accessories' Component={Accessories} />
     <Route path='/Blog' Component={Blog} />
     <Route path='/Contact' Component={Contact} />
+    <Route path='/Login' Component={Login} />
+    <Route path='/Register' Component={Register} />
    </Routes>
    </Router>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,29 +3,36 @@ import image from '../Assets/icons/psychiatry.svg'
 import search from '../Assets/icons/Search.svg'
 import person from '../Assets/icons/Person.svg'
 import mall from '../Assets/icons/local_mall.svg'
-import {Link} from 'react-router-dom'
-function Navbar(){
-    return(
+import { Link } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+
+function Navbar() {
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = './Login';
+        navigate(path);
+    }
+    return (
         <div className="nav">
             <div className="logo">
-                 <img src={image} alt=''></img>
-                 <h1>Brand Name</h1>
+                <img src={image} alt=''></img>
+                <h1>Brand Name</h1>
             </div>
             <div className='menu'>
-            <Link className='navlink' to="/TeaCollections">TEA COLLECTIONS</Link>
-            <Link className='navlink' to="/Accessories">ACCESSORIES</Link>
-            <Link className='navlink' to="/Blog">BLOG</Link>
-            
-            <Link className='navlink' to="/Contact">CONTACT US</Link>
+                <Link className='navlink' to="/TeaCollections">TEA COLLECTIONS</Link>
+                <Link className='navlink' to="/Accessories">ACCESSORIES</Link>
+                <Link className='navlink' to="/Blog">BLOG</Link>
+
+                <Link className='navlink' to="/Contact">CONTACT US</Link>
 
             </div>
             <div className='searchbar'>
-                  <button className='btn'><img src={search} alt='Button'></img></button>
-                  <button className='btn'><img src={person} alt=''></img></button>
-                  <button className='btn'><img src={mall} alt=''></img></button>
+                <button className='btn' ><img src={search} alt='Button'></img></button>
+                <button className='btn' onClick={routeChange}><img src={person} alt=''></img></button>
+                <button className='btn'><img src={mall} alt=''></img></button>
 
             </div>
         </div>
     )
-} 
+}
 export default Navbar;
